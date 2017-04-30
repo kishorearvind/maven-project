d7708c121627ea019118a3556e65196144b2938e
arvindaws <arvind.cloud.gcp@gmail.com>
2017-04-30 17:16:31 +0530

    update3

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -19,7 +19,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeTheOneBeingGreeted() {
-    String someone = "World of Titan";
+    String someone = "World of Cricket";
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
